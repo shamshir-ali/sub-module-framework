06cc5e642cf45731350be83619cc82d66e25c086
Shamshir Ali <shamshir.ali@confiz.com>
2023-12-29 16:38:53 +0500

    First method added.

---
+++ SubmoduleFramework/SubmoduleFramework/SubmoduleFramework.h
@@ -0,0 +1,18 @@
+//
+//  SubmoduleFramework.h
+//  SubmoduleFramework
+//
+//  Created by Shamshir Ali on 29/12/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SubmoduleFramework.
+FOUNDATION_EXPORT double SubmoduleFrameworkVersionNumber;
+
+//! Project version string for SubmoduleFramework.
+FOUNDATION_EXPORT const unsigned char SubmoduleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubmoduleFramework/PublicHeader.h>
+
+
---
+++ SubmoduleFramework/SubmoduleFramework/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  SubmoduleFramework
+//
+//  Created by Shamshir Ali on 29/12/2023.
+//
+
+import Foundation
+class Test {
+    func firstTest() {
+        print("this is first Test")
+    }
+}
